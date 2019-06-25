f2d4d26c3d6f919f0a0e4f5c4cc4ebb6195abef2
Ricardo Reis <ricardo.l.b.reis@gmail.com>
2019-06-25 08:36:08 -0300

    Update modelo.r
    Organizando o cabeçalho.

--- modelo.r
+++ modelo.r
@@ -1,11 +1,9 @@
-install.packages('mlbench')
-install.packages('car')
-library(mlbench)
-library(car)
-library(caTools)
-library(dplyr)
-library(plotly)
-
+################################################################################################
+#
+# Regressão Linear - Looqbox MeetUp
+#
+# CASE - BOSTON HOUSING
+#
 # crim - per capita crime rate by town
 # zn - proportion of residential land zoned for lots over 25,000 sq.ft
 # indus - proportion of non-retail business acres per town
@@ -20,71 +18,78 @@ library(plotly)
 # b 1000(B - 0.63)^2, where B is the proportion of blacks by town
 # lstat - percentage of lower status of the population
 # medv - median value of owner-occupied homes in USD 1000's
+#
+################################################################################################
+
+install.packages('mlbench')
+install.packages('car')
+library(mlbench)
+library(car)
+library(caTools)
+library(dplyr)
+library(plotly)
 
 data(BostonHousing)
 housing <- BostonHousing
 str(housing)
 
 summary(housing)
+cor(housing[-1],housing$medv)
+
+#Separando o dataset em treino e teste
+set.seed(123)
+
+split <- sample.split(housing,SplitRatio =0.75)
+trainData <- subset(housing,split==TRUE)
+testData <- subset(housing,split==FALSE)
 
 #Montando o modelo com todas as variaveis
-
 model1 <- lm(medv ~ crim + zn + indus + chas + nox + rm + age + dis + rad + tax + ptratio + b
-            + lstat, data = housing)
+            + lstat, data = trainData)
 vif(model1)
 summary(model1)
 
 #Retirando rad
 model2 <- lm(medv ~ crim + zn + indus + chas + nox + rm + age + dis + tax + ptratio + b
-            + lstat, data = housing)
+            + lstat, data = trainData)
 vif(model2)
 summary(model2)
 
 #Retirando age
 model3 <- lm(medv ~ crim + zn + indus + chas + nox + rm + dis + tax + ptratio + b
-             + lstat, data = housing)
+             + lstat, data = trainData)
 summary(model3)
 
 #Retirando b
 model4 <- lm(medv ~ crim + zn + indus + chas + nox + rm + dis + tax + ptratio 
-             + lstat, data = housing)
+             + lstat, data = trainData)
 summary(model4)
 
 #Retirando chas
 model5 <- lm(medv ~ crim + zn + indus + nox + rm + dis + tax + ptratio 
-             + lstat, data = housing)
+             + lstat, data = trainData)
 summary(model5)
 
 #Retirando indus
 model6 <- lm(medv ~ crim + zn  + nox + rm + dis + tax + ptratio 
-             + lstat, data = housing)
+             + lstat, data = trainData)
 summary(model6)
 
 #Retirando zn
-model6 <- lm(medv ~ crim + nox + rm + dis + tax + lstat, data = housing)
+model6 <- lm(medv ~ crim + nox + rm + dis + tax + lstat, data = trainData)
 summary(model6)
 
 #Retirando ptratio
-model7 <- lm(medv ~ crim + nox + rm + dis + tax + lstat, data = housing)
+model7 <- lm(medv ~ crim + nox + rm + dis + tax + lstat, data = trainData)
 summary(model7)
 
-#Separando o dataset em treino e teste
-
-set.seed(123)
-
-split <- sample.split(housing,SplitRatio =0.75)
-
-
-train <- subset(housing,split==TRUE)
-test <- subset(housing,split==FALSE)
-
 #Treinando
-modelTrain <- lm(medv ~ crim + nox + rm + dis + tax + lstat, data = train)
+modelTrain <- lm(medv ~ crim + nox + rm + dis + tax + lstat, data = trainData)
 summary(modelTrain)
 
-test$predicted.medv <- predict(modelTrain,test)
+testData$predicted.medv <- predict(modelTrain,testData)
 
-grafico <- test %>% 
+grafico <- testData %>% 
   ggplot(aes(medv,predicted.medv)) +
   geom_point(alpha=0.5) +
   xlab('MEDV Atual') +
@@ -92,5 +97,5 @@ grafico <- test %>%
 
 ggplotly(grafico)
 
-error <- test$predicted.medv-test$medv
-rmse <- sqrt(sum((error)^2)/length(test$medv))
+error <- testData$predicted.medv-testData$medv
+rmse <- sqrt(sum((error)^2)/length(testData$medv))
